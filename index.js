c3ea0207232164c34c2c3b542fe99f9762add63f
Alexandro <arvitaly@list.ru>
2017-04-20 04:35:40 +0700

    fix exports

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 Object.defineProperty(exports, "__esModule", { value: true });
+var Antigate_1 = require("./Antigate");
+exports.Antigate = Antigate_1.default;
 __export(require("./Antigate"));
 var constants_1 = require("./constants");
 exports.constants = constants_1.default;
--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
-import { default as Antigate } from "./Antigate";
+export { default as Antigate } from "./Antigate";
 export * from "./Antigate";
 export { default as constants } from "./constants";
